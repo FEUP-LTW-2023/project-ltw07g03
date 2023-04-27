9fd96fbd65be40b8ecedf6cd0b872d8baa7dddaf
MariaBeatrizCarneiro <up202107798@up.pt>
2023-04-27 16:32:08 +0100

    Criei uma página para editar o ticket

---
+++ Home Page/edit_ticket.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Edite um ticket" />
+	<title>Edit Ticket</title>
+	<link rel="stylesheet" type="text/css" href="style_new_ticket.css">
+</head>
+<header>
+	<a href=""><img id="logo" src="logo.png"></a>
+</header>
+<body>
+	<div class="container">
+		<h1>Editar Ticket</h1>
+		<form>
+            <input type="text" id="nome" name="nome" placeholder="Nome" required>
+            <input type="text" id="subject" name="subject" placeholder="Assunto" required>
+            <input type="text" id="departement" name="departement" placeholder="Departamento" required>
+            <textarea type="text" id="message" name="message" placeholder="Mensagem" rows="7" required></textarea>
+            <select id="priority" name="priority" required>
+                <option value="">--Escolha o nível de prioridade--</option>
+                <option value="Normal">Normal</option>
+                <option value="Urgente">Urgente</option>
+                <option value="Muito urgente">Muito urgente</option>
+            </select>
+			<input type="submit" value="Guardar">
+	    </form>
+</div>
+</body>
+</html>
+
+<?php
+
+?>
